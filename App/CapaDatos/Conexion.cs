e9bff38cdd71b672ab721f271557976b0b5f7c4b
JPoolB <jeanpoolbarr159@gmail.com>
2024-03-16 16:46:05 -0500

    first commit

---
+++ App/CapaDatos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    /// <summary>
+    /// Cadena de conexion a Bases de datos
+    /// </summary>
+    internal class Conexion
+    {
+        public static string Cn =
+            "Data Source=DESKTOP-QD9MU4U; " +
+            "Initial Catalog =DB_Clinica2; " +
+            "Integrated Security=true";
+    }
+}
